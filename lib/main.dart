45ef975e4668bc58c710f81e7a13166960745825
Muhammad Usama <imusamairshad@gmail.com>
2023-06-03 12:45:03 +0500

    main.dart is modified.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/home_page.dart';
+import 'package:whatsapp/screens/message_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(useMaterial3: true),
+    home: const HomePage(),
+    routes: {
+      MessageScreen.routeName: (ctx) => const MessageScreen(),
+    },
   ));
 }
