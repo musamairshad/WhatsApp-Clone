89a96053421cec5f3bc60c3a9eba0120dcbd7b15
Muhammad Usama <imusamairshad@gmail.com>
2023-06-22 19:13:33 +0500

    File is renamed.

---
+++ lib/data/chats_data.dart
@@ -0,0 +1,88 @@
+import 'package:whatsapp/models/chat.dart';
+
+List<Chat> chats = [
+  Chat(
+    name: 'Karthik',
+    picture: 'https://image.ibb.co/k0wVTm/profile_pic.jpg',
+    latestTimestamp: '10:00 AM',
+    lastChat: 'Or maybe not, let me check logistics.',
+  ),
+  Chat(
+    name: 'Alex',
+    picture: 'https://image.ibb.co/cA2oOb/alex_1.jpg',
+    latestTimestamp: '12:30 AM',
+    lastChat: 'Alright',
+  ),
+  Chat(
+    name: 'Bob',
+    picture: 'https://image.ibb.co/gSyTOb/bob_1.jpg',
+    latestTimestamp: '4:12 PM',
+    lastChat: 'I will look into it',
+  ),
+  Chat(
+    name: 'Luke',
+    picture: 'https://image.ibb.co/jOzeUG/luke_1.jpg',
+    latestTimestamp: '7:53 PM',
+    lastChat: 'Exactly my point!',
+  ),
+  Chat(
+    name: 'Bane',
+    picture: 'https://image.ibb.co/cBZPww/bane_1.jpg',
+    latestTimestamp: '1:09 PM',
+    lastChat: 'Not quite the same.',
+  ),
+  Chat(
+    name: 'Darth Vader',
+    picture: 'https://image.ibb.co/j4Ov3b/darth_vader_1.png',
+    latestTimestamp: 'Yesterday',
+    lastChat: 'Thanks',
+  ),
+  Chat(
+    name: 'Zach',
+    picture: 'https://image.ibb.co/b4kxGw/zach_1.jpg',
+    latestTimestamp: 'Yesterday',
+    lastChat: 'nothing',
+  ),
+  Chat(
+    name: 'Katie',
+    picture: 'https://image.ibb.co/eLVWbw/katie_1.jpg',
+    latestTimestamp: 'Wednesday',
+    lastChat: 'sure i\'ll take it from you',
+  ),
+  Chat(
+    name: 'Chloe',
+    picture: 'https://image.ibb.co/ncAa3b/chloe_1.jpg',
+    latestTimestamp: 'Wednesday',
+    lastChat: 'Take care, bye',
+  ),
+  Chat(
+    name: 'Kennith',
+    picture: 'https://image.ibb.co/fQKPww/kennith_1.jpg',
+    latestTimestamp: 'Monday',
+    lastChat: 'Not today',
+  ),
+  Chat(
+    name: 'Tara',
+    picture: 'https://image.ibb.co/dM6hib/tara_1.jpg',
+    latestTimestamp: 'Sunday',
+    lastChat: 'Whatever works for you!',
+  ),
+  Chat(
+    name: 'Gary',
+    picture: 'https://image.ibb.co/gsF8Ob/gary_1.jpg',
+    latestTimestamp: 'Sunday',
+    lastChat: 'Will get in touch',
+  ),
+  Chat(
+    name: 'Ash',
+    picture: 'https://image.ibb.co/iasYpG/ash_1.jpg',
+    latestTimestamp: '20/5/2023',
+    lastChat: 'Ok',
+  ),
+  Chat(
+    name: 'Zoey',
+    picture: 'https://image.ibb.co/nd0Wbw/zoey_1.jpg',
+    latestTimestamp: '18/5/2023',
+    lastChat: 'Lol',
+  ),
+];
