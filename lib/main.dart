2fd78b00164a559a1ee966044c9dcb44dfdb6c30
Muhammad Usama <imusamairshad@gmail.com>
2023-06-22 19:16:38 +0500

    main.dart is modified.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp/home_page.dart';
-import 'package:whatsapp/screens/message_screen.dart';
+import 'package:whatsapp/screens/messages.dart';
+import 'package:whatsapp/data/appbar.dart';
+import 'package:whatsapp/screens/communities.dart';
+import 'package:whatsapp/screens/chats.dart';
+import 'package:whatsapp/screens/status.dart.dart';
+import 'package:whatsapp/screens/calls.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(useMaterial3: true),
-    home: const HomePage(),
-    routes: {
-      MessageScreen.routeName: (ctx) => const MessageScreen(),
-    },
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const MyHomePage(),
+      routes: {
+        MessagesScreen.routeName: (ctx) => const MessagesScreen(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 4,
+      child: Scaffold(
+        appBar: appBar,
+        body: const TabBarView(
+          children: [
+            CommunitiesScreen(),
+            ChatsScreen(),
+            StatusScreen(),
+            CallsScreen(),
+          ],
+        ),
+      ),
+    );
+  }
 }
