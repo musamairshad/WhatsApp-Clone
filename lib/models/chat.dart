7203e054e4f0f0799292c5d6cd6c8c799bc136bb
Muhammad Usama <imusamairshad@gmail.com>
2023-06-22 19:16:57 +0500

    chat.dart is modified.

--- lib/models/chat.dart
+++ lib/models/chat.dart
@@ -3,15 +3,16 @@ import 'package:uuid/uuid.dart';
 const uuid = Uuid();
 
 class Chat {
+  final String id;
+  final String name;
+  final String picture;
+  final String latestTimestamp;
+  final String lastChat;
+
   Chat(
       {required this.name,
       required this.picture,
       required this.latestTimestamp,
       required this.lastChat})
       : id = uuid.v4();
-  final String id;
-  final String name;
-  final String picture;
-  final String latestTimestamp;
-  final String lastChat;
 }
