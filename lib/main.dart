d274f9e6a0fd2ceb91fc84fd189a33ca9fda1826
Muhammad Usama <imusamairshad@gmail.com>
2023-07-03 18:52:12 +0500

    main.dart is modified.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp/screens/messages.dart';
-import 'package:whatsapp/data/appbar.dart';
-import 'package:whatsapp/screens/communities.dart';
-import 'package:whatsapp/screens/chats.dart';
-import 'package:whatsapp/screens/status.dart.dart';
-import 'package:whatsapp/screens/calls.dart';
+import 'package:whatsapp/screens/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,32 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      home: const MyHomePage(),
+      home: const SplashScreen(),
       routes: {
         MessagesScreen.routeName: (ctx) => const MessagesScreen(),
       },
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 1,
-      length: 4,
-      child: Scaffold(
-        appBar: appBar,
-        body: const TabBarView(
-          children: [
-            CommunitiesScreen(),
-            ChatsScreen(),
-            StatusScreen(),
-            CallsScreen(),
-          ],
-        ),
-      ),
     );
   }
 }
